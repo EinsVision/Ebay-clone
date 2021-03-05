ca79f7e415be2ca5d40770d502853c8b1cfbca28
Changdae <einsvision@gmail.com>
2021-03-05 21:33:16 +0900

    update, empty, remove, add functions

--- ebay-clone/src/App.js
+++ ebay-clone/src/App.js
@@ -22,16 +22,32 @@ function App() {
   }
 
   const handleAddToCart = async (productId, quantity) => {
-    const item = await commerce.cart.add(productId, quantity);
-    setCart(item.cart);
+    const { cart } = await commerce.cart.add(productId, quantity);
+    setCart(cart);
   } 
+
+  const handleRemoveFromCart = async (productId) => {
+    const { cart } = await commerce.cart.remove(productId);
+    setCart(cart);
+  }
+
+  const handleEmptyCart = async () => {
+    const { cart } = await commerce.cart.empty();
+    setCart(cart);
+  }
+
+  const handleUpdateCartQty = async (productId, quantity) => {
+    const { cart } = await commerce.cart.update(productId, { quantity });
+
+    setCart(cart);
+  }
 
   useEffect( () => {
     fetchProducts();
     fetchCart();
   }, []);
 
-   console.log(cart);
+  //  console.log(cart);
   
   return (
     <Router>
