420bb8f3c008758f3d3fe2ade65c3037e7568222
Changdae <einsvision@gmail.com>
2021-03-05 10:55:16 +0900

    importing products on the website using jsx

--- ebay-clone/src/components/Products/Products.jsx
+++ ebay-clone/src/components/Products/Products.jsx
@@ -3,8 +3,8 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 
 const products = [
-  {id: 1, name: 'Shoes', description: 'Running shoes', price: '$5'},
-  {id: 2, name: 'Macbook', description: 'Apple Macbook', price: '$10'},
+  {id: 1, name: 'Shoes', description: 'Running shoes', price: '$5', image: 'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/i1-9cfea66d-b519-4b29-8e43-ce4164e8c558/adapt-bb-2-tie-dye-basketball-shoe-vdFwKS.jpg'},
+  {id: 2, name: 'Macbook', description: 'Apple Macbook', price: '$10', image: 'https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/macbook-air-space-gray-config-201810?wid=1078&hei=624&fmt=jpeg&qlt=80&.v=1603332211000'},
 ];
 
 function Products() {
