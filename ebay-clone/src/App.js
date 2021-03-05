2c0fafaf30ffff1e4c9ff6f5ae43d1392c4a329a
Changdae <einsvision@gmail.com>
2021-03-05 14:08:50 +0900

    Products component display from commerce js product

--- ebay-clone/src/App.js
+++ ebay-clone/src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Navbar from './components/Nav/Navbar';
 import Products from './components/Products/Products';
+import {commerce} from './lib/commerce';
 
 function App() {
+  const [products, setProducts] = useState([]);
+  
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  }
+
+  useEffect( () => {
+    fetchProducts();
+  }, []);
+
+  // console.log(products);
+  
   return (
     <div className="app">
       <Navbar />
-      <Products />
+      <Products products={products} />
     </div>
   );
 }
