c32092fe9b5e720731b282baec8e0ebc8d95ebf5
Changdae <einsvision@gmail.com>
2021-03-06 21:04:00 +0900

    building address form using react-hook-form

--- ebay-clone/src/components/CheckoutForm/AddressForm.jsx
+++ ebay-clone/src/components/CheckoutForm/AddressForm.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { InputLabel, Select, MenuItem, Button, Grid, Typography} from '@material-ui/core';
+import { useForm, FormProvider } from 'react-hook-form';
+import CustomTextField from '../CheckoutForm/Checkout/CustomTextField';
 
 function AddressForm() {
+
+  const methods = useForm();
   return (
-    <div>
-      AddressForm
-    </div>
+    <>
+      <Typography variant='h6' gutterBottom>Shipping Address</Typography>
+      <FormProvider {...methods}>
+        <form onSubmit=''>
+          <Grid container spacing={3}>
+            <CustomTextField required name='firstName' label='Fist name' />
+            <CustomTextField required name='lastname' label='Last name' />
+            <CustomTextField required name='address1' label='Address' />
+            <CustomTextField required name='email' label='Email' />
+            <CustomTextField required name='city' label='City' />
+            <CustomTextField required name='zip' label='ZIP / Postal code' />
+          </Grid>
+        </form>
+      </FormProvider>
+    </>
   )
 }
 
